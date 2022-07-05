db318bfdaf30c20d08bd9a31b790d600d6622f22
David Chuvik <dchuvik@gmail.com>
2022-07-05 15:59:00 -0400

    using API of Kanye and Ron Swonson qoutes and simulated a conversation

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                kanye();
+                Ron();
+            }
+        }
+        public static void kanye()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest/";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine($"Kanye West: '{kanyeQuote}'\n");
+        }
+        public static void Ron()
+        {
+            var client2 = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client2.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine($"Ron Swonson: {ronQuote}\n");
+        }
+
+    }
+}
